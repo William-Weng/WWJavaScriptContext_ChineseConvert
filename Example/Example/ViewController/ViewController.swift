01ed0dcf356c1ad2a715f801f845280df2062efe
William.Weng <William.Weng@iit.com.hk>
2023-09-12 13:59:40 +0800

    - v1.0.0

---
+++ Example/Example/ViewController/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by William.Weng on 2023/9/11.
+//  ~/Library/Caches/org.swift.swiftpm/
+
+import UIKit
+import WWJavaScriptContext
+import WWJavaScriptContext_ChineseConvert
+
+final class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    /// 簡體 => 正體
+    /// - Parameter sender: UIBarButtonItem
+    @IBAction func convert_zh_TW(_ sender: UIBarButtonItem) {
+                
+        let traditionalChinese = WWJavaScriptContext.ChineseConvert.shared.traditionalChinese(source: myLabel.text)
+        myLabel.text = traditionalChinese
+    }
+    
+    /// 繁體 => 簡體
+    /// - Parameter sender: UIBarButtonItem
+    @IBAction func convert_zh_CN(_ sender: UIBarButtonItem) {
+        
+        let simplifiedChinese = WWJavaScriptContext.ChineseConvert.shared.simplifiedChinese(source: myLabel.text)
+        myLabel.text = simplifiedChinese
+    }
+}
+
