f11c05c5ba50e0d02f4f18b29581ca2fe17bfd88
William Weng <linuxice0609@gmail.com>
2025-03-16 16:05:10 +0800

    - iOS 15 / Swift 5.7

--- Package.swift
+++ Package.swift
@@ -1,21 +1,21 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "WWJavaScriptContext+ChineseConvert",
+    name: "WWJavaScriptContext_ChineseConvert",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v15),
     ],
     products: [
-        .library(name: "WWJavaScriptContext+ChineseConvert", targets: ["WWJavaScriptContext+ChineseConvert"]),
+        .library(name: "WWJavaScriptContext_ChineseConvert", targets: ["WWJavaScriptContext_ChineseConvert"]),
     ],
     dependencies: [
         .package(url: "https://github.com/William-Weng/WWJavaScriptContext", from: "1.0.3"),
     ],
     targets: [
-        .target(name: "WWJavaScriptContext+ChineseConvert", dependencies: ["WWJavaScriptContext"], resources: [.process("Script"), .copy("Privacy")]),
+        .target(name: "WWJavaScriptContext_ChineseConvert", dependencies: ["WWJavaScriptContext"], resources: [.process("Script"), .copy("Privacy")]),
     ],
     swiftLanguageVersions: [
         .v5
